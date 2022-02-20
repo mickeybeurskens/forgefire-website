5c1eff87db74d72f5a77a4ce9a8b5b6507888c25
mickeybeurskens <mickeybeurskens@gmail.com>
2022-02-20 17:09:02 +0100

    Add bootstrap styling to header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,31 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Container } from 'react-bootstrap'
 
 const Header = () => {
+  const headerPageNames = [
+    {name: 'Home', link: '/'},
+    {name: 'Blogs', link: '/blogs'},
+    {name: 'Consultancy', link: '/consultancy'},
+  ]
+
+  const currentPath = useRouter().pathname
   return (
-    <header>
-      <Link href='/'>
-        This is a header
-      </Link>
-    </header>
+    <Container>
+        <header className='d-flex justify-content-center py-3'>
+        <ul className="nav nav-pills">
+        {headerPageNames.map((pageInfo, index) => (
+          <li key={index} className='nav-item'>
+            <Link href={pageInfo.link}>
+              <a className={'nav-link ' + (pageInfo.link===currentPath ? 'active' : '')}>
+                {pageInfo.name}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      </header>
+    </Container>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import loadBlogs from '../utils/blog'
 import styles from '../styles/Home.module.scss'
-import Badge from 'react-bootstrap/Badge';
+import Badge from 'react-bootstrap/';
 
 const Home = (props) => {
-  console.log('home', props)
   return (
     <div>
       <p>Hello Blog Home</p>
-      <Badge bg="secondary">BADGE!</Badge>
       <div className='posts'>
         {props.blogs.map((blog, index) => (
           <h3 key={index}>{blog.front_matter.title}</h3>
