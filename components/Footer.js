742b3c01bc765e5182ab7aa8883605e24dd98c5d
mickeybeurskens <mickeybeurskens@gmail.com>
2022-02-21 12:06:25 +0100

    Add footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,24 +1,47 @@
-import Container from 'react-bootstrap/Container'
+import Link from 'next/link'
+import Image from 'next/image'
+import {useRouter} from 'next/router'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+const MenuItems = () => {
+  const footerPageNames = [
+    {name: 'Home', link: '/'},
+    {name: 'Blogs', link: '/blogs'},
+    {name: 'Consultancy', link: '/consultancy'},
+  ]
+  
+  const currentPath = useRouter().pathname
+  return (
+  <Navbar className='ol-md-4 justify-content-end list-unstyled d-flex'>
+  <Nav >
+  {footerPageNames.map((pageInfo, index) => (
+    <Link key={index} href={pageInfo.link}>
+      <a className={'nav-link ' + (pageInfo.link===currentPath ? 'active' : '')}>
+        {pageInfo.name}
+      </a>
+    </Link>
+  ))}
+  </Nav>
+  </Navbar>
+  )
+}
+
+const CopyRight = () => (
+  <div className="col-md-4 d-flex align-items-center">
+    <Link href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      <a><Image src='/logo_forge_fire.svg' alt='forge fire logo' width="40" height="40"/></a>
+    </Link>
+    <span className="text-muted">&copy; {new Date().getFullYear().toString()} Forge Fire</span>
+  </div>
+)
 
 const Footer = () => {
-
-{/* <div className="container">
-  <footer className="py-3 my-4">
-    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-      <li className="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-      <li className="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-    </ul>
-    <p class="text-center text-muted">&copy; 2021 Company, Inc</p>
-  </footer>
-</div> */}
   return (
-    <Container>
-      <footer>
-        footer
-      </footer>
-    </Container>
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <CopyRight/>
+    <MenuItems/>
+    </footer>
   )
 }
 
--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
       ))}
     </Nav>
     </Container>
-    </Navbar>
+    </Navbar> 
   )
 }
 
