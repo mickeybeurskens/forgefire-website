e38a9cb62577c1e553dac6de9760cce14006416e
mickeybeurskens <mickeybeurskens@gmail.com>
2022-03-07 18:25:42 +0100

    Add swirl to home page background

---
+++ components/Swirl.js
@@ -0,0 +1,11 @@
+import styles from "../styles/swirl.module.scss"
+
+const Swirl = () => {
+  return (
+    <>
+      <div className={styles.swirl}/>
+    </>
+  )
+}
+
+export default Swirl
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
+import Swirl from '../components/Swirl'
 import PostOverview from '../components/PostOverview'
 import ServiceOverview from '../components/home/ServiceOverview'
 import TestimonialsDots from '../components/home/TestimonialsDots'
 import { getAllPosts } from '../lib/post'
+import styles from "../styles/home.module.scss"
 
 const Home = (props) => {
   return (
     <main>
       <Hero/>
-      <PostOverview posts={props.posts} numberOfBlogs={3}/>
-      <ServiceOverview/>
-      <TestimonialsDots/>
+      <Swirl/>
+      <div className={styles.alt_bg}>
+        <PostOverview posts={props.posts} numberOfBlogs={3}/>
+        <ServiceOverview/>
+        <TestimonialsDots/>
+      </div>
     </main>
   )
   }
