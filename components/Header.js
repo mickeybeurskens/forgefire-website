5d51d2619281ef7ceffffbde70d24dd00cac1e50
mickeybeurskens <mickeybeurskens@gmail.com>
2022-03-07 16:04:43 +0100

    Finish header

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import styles from '../styles/header.module.scss'
+import configs from '../lib/configs.js'
+
 
 const Header = () => {
   const headerPageNames = [
@@ -13,14 +15,19 @@ const Header = () => {
     {name: 'Consultancy', link: '/consultancy'},
     {name: 'Contact', link: '/contact'}
   ]
+  const socials_size = 35;
 
   const currentPath = useRouter().pathname
   return (
     <Navbar className={styles.navigation} variant="light">
     <Container>
     <div className={styles.logo}>
-      <Image src='/logo_forge_fire.svg' 
-        alt='forge fire logo' width="55" height="55"/>
+      <Link href='/'>
+        <a>
+          <Image src='/logo_forge_fire.svg' 
+            alt='forge fire logo' width="55" height="55"/>
+        </a>
+      </Link>
     </div>
     <Nav className="me-auto">
       {headerPageNames.map((pageInfo, index) => (
@@ -31,6 +38,20 @@ const Header = () => {
         </Link>
       ))}
     </Nav>
+    <div className={styles.socials}>
+      <a href={configs.github_url}>
+        <Image src='/github_logo.png'
+          alt='github logo' width={socials_size} height={socials_size}/>
+      </a>
+    </div>
+    <div className={styles.socials}>
+      <a href={configs.linkedin_url}>
+        <Image src='/linkedin_logo.png'
+          alt='linkedin logo' width={socials_size} height={socials_size}/>
+      </a>
+    </div>
+
+
     </Container>
     </Navbar> 
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../styles/bootstrap_custom.scss'
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.scss'
 
 const App = ({Component, pageProps}) => {
