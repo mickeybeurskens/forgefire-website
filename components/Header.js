24da184c17bf8680d5e07bb7f554d6e633c34f4b
mickeybeurskens <mickeybeurskens@gmail.com>
2022-03-15 17:12:30 +0100

    Mobile compatible header and footer

--- components/Header.js
+++ components/Header.js
@@ -20,30 +20,31 @@ const Header = () => {
 
   const currentPath = useRouter().pathname
   return (
-    <Navbar className={styles.navigation} variant="light">
+    <Navbar expand='md' className={styles.navigation} variant="light">
     <Container>
     <Logo size={55}/>
-    <Nav className="me-auto">
-      {headerPageNames.map((pageInfo, index) => (
-        <Link key={index} href={pageInfo.link}>
-          <a className={styles.text + ' nav-link ' + (pageInfo.link===currentPath ? 'active' : '')}>
-            {pageInfo.name}
-          </a>
-        </Link>
-      ))}
-    </Nav>
-    <div className={styles.socials}>
-      <a href={configs.github_url}>
-        <Image src='/github_logo.png'
-          alt='github logo' width={socials_size} height={socials_size}/>
-      </a>
-    </div>
-    <div className={styles.socials}>
-      <a href={configs.linkedin_url}>
-        <Image src='/linkedin_logo.png'
-          alt='linkedin logo' width={socials_size} height={socials_size}/>
-      </a>
-    </div>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse>
+      <Nav className="me-auto">
+        {headerPageNames.map((pageInfo, index) => (
+          <Link key={index} href={pageInfo.link}>
+            <a className={styles.text + ' nav-link ' + (pageInfo.link===currentPath ? 'active' : '')}>
+              {pageInfo.name}
+            </a>
+          </Link>
+        ))}
+      </Nav>
+      <div className={styles.socials}>
+        <a href={configs.github_url}>
+          <Image src='/github_logo.png'
+            alt='github logo' width={socials_size} height={socials_size}/>
+        </a>
+        <a href={configs.linkedin_url}>
+          <Image src='/linkedin_logo.png'
+            alt='linkedin logo' width={socials_size} height={socials_size}/>
+        </a>
+      </div>
+    </Navbar.Collapse>
     </Container>
     </Navbar> 
   )
