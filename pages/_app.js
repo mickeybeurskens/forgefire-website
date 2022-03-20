a26bde5cbe0fca77cdb5df1920491c321334daa2
mickeybeurskens <mickeybeurskens@gmail.com>
2022-03-20 20:47:27 +0100

    Add metadata to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
+import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../styles/bootstrap_custom.scss'
 import '../styles/global.scss'
+import configs from '../lib/configs'
 
 const App = ({Component, pageProps}) => {
   return <>
+    <Head>
+      <title>Forge Fire</title>
+      <meta name="author" content={configs.author}/>
+      <meta name="description" content={configs.site_description}/>
+    </Head>
     <Header/>
     <Component {...pageProps} />
     <Footer/>
