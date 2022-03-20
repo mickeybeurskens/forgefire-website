b625c649f7e5f769998c936eb973ff77d3dc72a8
mickeybeurskens <mickeybeurskens@gmail.com>
2022-03-20 18:55:43 +0100

    Style posts

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { getAllPosts, getPostBySlug } from '../../lib/post'
+import Contact from '../../components/Contact'
 import ReactMarkdown from 'react-markdown'
 import styles from '../../styles/posts.module.scss'
+import Divider from '../../components/Divider'
+
+const PostHero = ({img_path}) => {
+  return (
+      <div style={{backgroundImage: 'url(' + img_path + ')',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: 200}}/>
+  )
+}
 
 const Post = ({post}) => {
   const router = useRouter()
   const { id } = router.query
   return (
-    <div className={styles.posts}>
-      <p>{post.author} and {id} </p>
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+    <div>
+      {/* <PostHero img_path={post.img_path}/> */}
+      <div className={styles.posts}>
+        <p>{post.author} and {id} </p>
+        <ReactMarkdown>{post.content}</ReactMarkdown>
+      </div>
+      <Divider/>
+      <Contact/>
     </div>
   )
 }
@@ -21,6 +38,7 @@ const getStaticProps = async ({ params }) => {
     'date',
     'slug',
     'author',
+    'img_path',
     'content'
   ])
 return {
